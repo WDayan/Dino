878b06662e0df54aaa0f56310edf0a0e95411216
brunoorsoletta <br-bruno@hotmail.com>
2014-10-17 20:39:04 -0300

    imprementação do } while

---
+++ repeticao.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.io.*;
+
+class repeticao 
+{
+	public double repete(int cursor, int b, )
+	{
+		do
+		{
+			
+		} while (a < b);
+	}
+	
+    public static void main(String args[])
+	{
+		
+	}
+}
